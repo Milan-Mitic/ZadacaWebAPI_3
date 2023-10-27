74876e69e29f5726f6ef4d746b19728061a7b3a5
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-10-27 12:59:00 +0200

    Add files via upload

---
+++ ZadacaWebApi_3/Controllers/BooksController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using ZadacaWebApi_3.Data;
+using ZadacaWebApi_3.Models;
+
+
+namespace ZadacaWebApi_3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet("allBooks")]
+        public ActionResult GetAllBooks()
+        {
+            return Ok(StaticDb.books);
+        }
+
+        [HttpGet("{index}")]
+
+        public ActionResult<Book> GetBook(int index) 
+        {
+            if (index >= 0 && index < StaticDb.books.Count)
+            {
+                return StaticDb.books[index];
+            }
+            else
+            {
+                return NotFound("The book not found.");
+            }
+        }
+
+        [HttpGet("searchBook")]
+        public ActionResult<Book> SearchBook([FromQuery] string author,[FromQuery] string title) 
+        {
+            var book = StaticDb.books.FirstOrDefault(b => b.Author == author && b.Title == title);
+
+            if(book == null)
+            {
+                return NotFound("The book not found.");
+            }
+            return Ok(book);
+        }
+
+        [HttpPost("addBook")]
+
+        public ActionResult<Book> AddBook([FromBody] Book book) 
+        {
+            StaticDb.books.Add(book);
+            return Ok(book);
+        }
+
+        [HttpPost("titles")]
+        public ActionResult<IEnumerable<string>> GetBookTitles([FromBody] List<Book> newBooks) 
+        {
+
+            StaticDb.books.AddRange(newBooks);
+            var addedTitles = newBooks.Select(b => b.Title).ToList();
+            return Ok(addedTitles);
+        }
+    }
+}
---
+++ ZadacaWebApi_3/Data/StaticDb.cs
@@ -0,0 +1,16 @@
+﻿using ZadacaWebApi_3.Models;
+
+namespace ZadacaWebApi_3.Data
+{
+    public class StaticDb
+    {
+        public static List<Book> books = new List<Book>()
+        {
+            new Book() { Author = "Author1", Title = "Title1"},
+            new Book() { Author = "Author2", Title = "Title2"},
+            new Book() { Author = "Author3", Title = "Title3"},
+            new Book() { Author = "Author4", Title = "Title4"},
+            new Book() { Author = "Author5", Title = "Title5"}
+        };
+    }
+}
